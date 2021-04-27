ef8f78b601f7ad29838a2abfb419f096b00a6cae
JairJap28 <jairmunevar@gmail.com>
2021-04-27 13:24:10 -0500

    Wrapp stats item in Paper component

--- src/components/TodoListStats.js
+++ src/components/TodoListStats.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { todoListStatsState } from "../Recoil/Selector";
-import TodoStatsItem from './TodoStatsItem';
+import TodoStatsItem from "./TodoStatsItem";
 
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import Grid from '@material-ui/core/Grid';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(() => ({
   root: {
     bottom: 0,
-    position: 'absolute'
+    position: "absolute",
+    margin: '10px 10px',
+    width: 'calc(100% - 30px)'
   }
 }));
 
 const TodoListStats = () => {
-  const classes = useStyles();  
+  const classes = useStyles();
 
   const {
     totalNum,
@@ -26,11 +28,26 @@ const TodoListStats = () => {
   const formattedPercentCompleted = Math.round(percentCompleted);
 
   return (
-    <Grid container className={classes.root}>
-      <TodoStatsItem title="Total items:" amount={totalNum} xs={12} sm={12}/>
-      <TodoStatsItem title="Items completed:" amount={totalCompletedNum}/>
-      <TodoStatsItem title="Items not completed:" amount={totalUnCompletedNum}/>
-      <TodoStatsItem title="Percent completed:" amount={formattedPercentCompleted} xs={12} sm={12}/>
+    <Grid container className={classes.root} spacing={1}>
+      <TodoStatsItem title="Total items:" amount={totalNum} xs={6} sm={6} />
+      <TodoStatsItem
+        title="Percent completed:"
+        amount={formattedPercentCompleted}
+        xs={6}
+        sm={6}
+      />
+      <TodoStatsItem
+        title="Items completed:"
+        amount={totalCompletedNum}
+        xs={6}
+        sm={6}
+      />
+      <TodoStatsItem
+        title="Items not completed:"
+        amount={totalUnCompletedNum}
+        xs={6}
+        sm={6}
+      />
     </Grid>
   );
 };
--- src/components/TodoStatsItem.js
+++ src/components/TodoStatsItem.js
@@ -1,12 +1,26 @@
 import React from "react";
 
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper';
+
+const useStyle = makeStyles(() => ({
+  root: {
+    textAlign: "center"
+  },
+  paper: {
+    padding: 10
+  }
+}));
 
 const TodoStatsItem = ({ title, amount, xs = 12, sm = 3, lg = 3 }) => {
+  const classes = useStyle();
   return (
-    <Grid item xs={xs} sm={sm} lg={lg}>
-      {title}
-      {amount}
+    <Grid item xs={xs} sm={sm} lg={lg} className={classes.root}>
+      <Paper elevation={3} className={classes.paper}>
+        {title}
+        {amount}
+      </Paper>
     </Grid>
   );
 };
